bda00dcafd896f331100d67b5589ff1fdce16580
Indy Khare <ikhare@convex.dev>
2024-08-27 17:10:32 -0700

    Add minimal sign in screen

---
+++ app/src/main/java/dev/convex/workouttracker/ui/SignInScreen.kt
@@ -0,0 +1,42 @@
+package dev.convex.workouttracker.ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import dev.convex.workouttracker.ui.theme.WorkoutTrackerTheme
+
+@Composable
+fun SignInScreen(
+) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text(
+            text = "Workout\nTracker",
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.displayMedium,
+        )
+        // TODO: Kick off auth flow
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Sign In")
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 480)
+@Composable
+fun SignInScreenPreview() {
+    WorkoutTrackerTheme {
+        SignInScreen()
+    }
+}
