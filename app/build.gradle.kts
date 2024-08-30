1e430a7e2bb036733673f20e91d380e35c2ba1d5
Indy Khare <ikhare@convex.dev>
2024-08-30 15:59:36 -0700

    Theme generated by material theme generator:
    http://material-foundation.github.io?primary=%23FFDE3F&displayFont=Poppins&colorMatch=true

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -84,6 +84,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.ui.text.google.fonts)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
