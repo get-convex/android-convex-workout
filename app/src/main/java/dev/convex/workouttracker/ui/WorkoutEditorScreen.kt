0fe05594ddc1c49b8a2052d12b22f681a0912f3e
Indy Khare <ikhare@convex.dev>
2024-09-04 17:28:07 -0700

    Switch date picker to a dialog

--- app/src/main/java/dev/convex/workouttracker/ui/WorkoutEditorScreen.kt
+++ app/src/main/java/dev/convex/workouttracker/ui/WorkoutEditorScreen.kt
@@ -1,18 +1,18 @@
 package dev.convex.workouttracker.ui
 
-import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material3.Button
 import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.DatePickerState
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
@@ -21,9 +21,9 @@ import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
@@ -33,10 +33,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Popup
 import dev.convex.workouttracker.models.Workout
 import dev.convex.workouttracker.ui.theme.WorkoutTrackerTheme
 import kotlinx.datetime.Instant
@@ -169,30 +167,24 @@ fun DatePickerDocked(datePickerState: DatePickerState = rememberDatePickerState(
             modifier = Modifier
                 .fillMaxWidth()
                 .height(64.dp)
+                .clickable { showDatePicker = true }
         )
 
         if (showDatePicker) {
-            Popup(
+            DatePickerDialog(
                 onDismissRequest = { showDatePicker = false },
-                alignment = Alignment.TopStart
+                confirmButton = {
+                    TextButton(onClick = { showDatePicker = false }) {
+                        Text("OK")
+                    }
+                },
             ) {
-                Box(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .offset(y = 64.dp)
-                        .shadow(elevation = 4.dp)
-                        .background(MaterialTheme.colorScheme.surface)
-                        .padding(16.dp)
-                ) {
-                    DatePicker(
-                        state = datePickerState,
-                        showModeToggle = false
-                    )
-                }
+                DatePicker(state = datePickerState)
             }
         }
     }
 }
+
 
 private fun convertMillisToDate(millis: Long): String =
     Instant.fromEpochMilliseconds(millis).toLocalDateTime(TimeZone.UTC).date.toString()
